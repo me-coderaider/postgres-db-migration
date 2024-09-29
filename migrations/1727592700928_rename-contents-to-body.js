627f615eebc67b40cab5291ff2a68b152cea6775
me-coderaider <kashyappankajnsp@gmail.com>
2024-09-29 12:24:50 +0530

    added sql command in 2nd migration file

--- migrations/1727592700928_rename-contents-to-body.js
+++ migrations/1727592700928_rename-contents-to-body.js
@@ -8,11 +8,21 @@ exports.shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.up = (pgm) => {};
+exports.up = (pgm) => {
+    pgm.sql(`
+        ALTER TABLE comments
+        RENAME COLUMN contents to body;
+    `)
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+    pgm.sql(`
+        ALTER TABLE comments
+        RENAME COLUMN body to contents;
+    `)
+};
