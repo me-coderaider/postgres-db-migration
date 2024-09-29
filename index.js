f81d468610a8bd2de0c3b35866dcfd9f23f04fa8
me-coderaider <kashyappankajnsp@gmail.com>
2024-09-29 15:51:06 +0530

    creating a simple node server to replicate schema & data migration

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const pg=require('pg');
+
+const pool=new pg.Pool({
+    host:'localhost',
+    port:5432,
+    database:'socialnetwork',
+    user:'postgres',
+    password:'postgres'
+});
+
+pool.query('SELECT 1 + 1;').then((res)=> console.log(res));
